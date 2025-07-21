7ce852e98a79eb160e20d63f9a525be8409bc89a
Semaj Wiggins <semaj.wiggins@hgs.hiddengeniusproject.org>
2025-07-21 14:40:42 -0700

    CreateClass

--- Python/Week1/Week2/Week 6/CreateClass.py
+++ Python/Week1/Week2/Week 6/CreateClass.py
@@ -1,76 +1,74 @@
 # 1. Create a class to represent a Video Game or Movie Collection
 # 2. Create a constructor method __init__()
 # 3. Create a list for the video games and movies each
-# 4. Create a instance variable for the user's favorite movie movie and video game respectively
+# 4. Create a instance variable for the user's favorite movie and video game respectively
 # 5. Create the following functions for your class
-# - A fucntion to display all movies
-# - A function to display all the video games
+# - A function to display all the movies
+# - A function to display all the video game
 # - A function to add a movie/video game
-# - A function to remove a movie/video game
-# - A function ot select a favorite movie/video game
-# 6. Create a seperate tester.py file to test your code
+# - A function to remove a move/video game
+# - A function to select a favorite video game and or movie
+# 6. Crate a seperate tester.py file to test your code
 
 class Collection:
-    def __init__(self, movieList, gameList): # Organizing, taking all the objects in, and creating the list
-        self.movieList = []
-        self.gameList = []
-        self.favgame = ""
-        self.favmovie = ""
-
+    def __init__(self, movieList, gameList):
         self.movieList = movieList
         self.gameList = gameList
+        self.favGame = ""
+        self.favMovie = ""
 
-        def AddGame(self, game):
-            if game in self.gameList:
-                print("Game is already in collection")
-            else:
-                self.gameList.append(game)
+    def add_game(self, game):
+        if game in self.gameList:
+            print("Game is already in collection")
+        else:
+            self.gameList.append(game)
 
-        def AddMovie(self, movie):
-            if movie in self.movieList:
-                print("Movie is already in collection")
+    def add_movie(self, movie):
+        if movie in self.movieList:
+            print("Movie is already in collection")
+        else:
             self.movieList.append(movie)
 
-        def RemoveGame(self, game):
-            if game not in self.gameList:
-                self.gameList.remove(game)
-            else:
-                print("Game Not Found")
-        
-        def RemovieMovie(self, movie):
-            if movie not in self.gameList:
-                self.movieList.remove(movie)
-            else:
-                print("Movie Not Found")
+    def remove_game(self, game):
+        if game in self.gameList:
+            self.gameList.remove(game)
+        else:
+            print("Game Not Found")
 
-        def display_Movies(self):
-            print("Movies in your collection:")
+    def remove_movie(self, movie):
+        if movie in self.movieList:
+            self.movieList.remove(movie)
+        else:
+            print("Movie Not Found")
+
+    def display_movies(self):
+        print("Movies in your collection:")
         for movie in self.movieList:
             print(f"- {movie}")
 
-        def display_Games(self):
-            print("Video Games in your collection:")
+    def display_games(self):
+        print("Video Games in your collection:")
         for game in self.gameList:
-            print(f"-{game}")
+            print(f"- {game}")
 
-        def DisplayFavGame(self):
-            print(f'Fav Game{self.favGame}')
+    def display_fav_game(self):
+        print(f"Favorite Game: {self.favGame}")
 
-        def DisplayFavMovie(self):
-            print(f'Fav Game {self.favMovie}')
+    def display_fav_movie(self):
+        print(f"Favorite Movie: {self.favMovie}")
 
-        def DisplayCollection(self):
-            self.DisplayMovies()
-            self.DisplayFavMovies()
-            self.DisplayGames()
-            self.DisplayFavGames()
-        
-        def SetFavMovie(self, movie):
-            if movie not in self.movieList:
-                self.AddMovie(movie)
-            self.favMovie = movie
+    def display_collection(self):
+        self.display_movies()
+        self.display_fav_movie()
+        self.display_games()
+        self.display_fav_game()
 
-        def SetFavGame(self, game):
-            if movie not in self.movieList:
-                self.AddGame(game)
-            self.favGame(game)
+    def set_fav_movie(self, movie):
+        if movie not in self.movieList:
+            self.add_movie(movie)
+        self.favMovie = movie
+
+    def set_fav_game(self, game):
+        if game not in self.gameList:
+            self.add_game(game)
+        self.favGame = game
